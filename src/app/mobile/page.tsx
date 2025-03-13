117d4a03ba5fdbba219635867417442299e35f2a
thas93 <a.thas.dev93@gmail.com>
2025-03-13 15:30:55 +0100

    section-mobile

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -6,9 +6,7 @@ const Mobile = () => {
   return (
     <div className="flex flex-col justify-center items-center w-screen min-h-screen bg-auto bg-fixed backdrop-blur-sm">
       <div className="flex justify-center items-center gap-10">
-        
         <h1 className="text-4xl font-black text-orange-500 tracking-[0.3rem]">
-          
           TELEPHONES
         </h1>
       </div>
@@ -19,15 +17,22 @@ const Mobile = () => {
             ECRAN
           </h1>
           <Image
-            src="/images/parts/ecran.png"
+            src="/images/parts/screen.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-            Le remplacement de l'écran d'un téléphone est l'une des réparations les plus fréquentes, souvent nécessaire en cas de chute, de choc ou d'exposition à des conditions extrêmes. Même si l'écran ne présente pas de fissures visibles, des dommages internes causés par des impacts répétés, une exposition prolongée à la chaleur ou à l'humidité peuvent altérer sa sensibilité tactile ou provoquer l'apparition de zones noires. Ces problèmes peuvent rendre l'appareil difficile, voire impossible, à utiliser. Il est donc essentiel de faire vérifier et remplacer l'écran rapidement pour garantir le bon fonctionnement du téléphone et éviter une détérioration supplémentaire.
+            Le remplacement d’un écran de téléphone est l’une des interventions
+            les plus fréquentes, représentant près de 80 % des réparations
+            effectuées. Même si l’écran paraît en bon état, des chocs
+            successifs, une exposition excessive à la chaleur ou l’infiltration
+            d’humidité peuvent altérer la réactivité tactile ou provoquer un
+            affichage totalement noir. Il est donc essentiel d’agir rapidement
+            pour éviter une aggravation du problème et garantir le bon
+            fonctionnement de l’appareil.{" "}
           </p>
         </div>
 
@@ -36,15 +41,26 @@ const Mobile = () => {
             APPAREIL PHOTO
           </h1>
           <Image
-            src="/images/parts/cam.png"
+            src="/images/parts/lens.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-            Le remplacement de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            Le remplacement de l'appareil photo d'un téléphone est une
+            réparation de plus en plus courante, notamment avec l'utilisation
+            intensive des fonctionnalités photographiques. Même si l'objectif
+            semble intact, des chocs, une exposition à l'humidité ou une
+            accumulation de poussière peuvent endommager les capteurs ou les
+            lentilles, entraînant des photos floues, des taches sur les images
+            ou un dysfonctionnement complet de la fonction photo. Ces problèmes
+            peuvent considérablement réduire la qualité des clichés et nuire à
+            l'expérience utilisateur. Il est donc important de diagnostiquer et
+            de remplacer l'appareil photo rapidement pour retrouver des
+            performances optimales et préserver la capacité à capturer des
+            moments précieux.
           </p>
         </div>
 
@@ -53,33 +69,57 @@ const Mobile = () => {
             BATTERIE
           </h1>
           <Image
-            src="/images/parts/battery.png"
+            src="/images/parts/batery.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-           la batterie représentant une part importante des demandes en atelier. Même si le téléphone semble fonctionner normalement, une dégradation progressive de la batterie, due à des cycles de charge répétés, une surchauffe ou une utilisation intensive, peut entraîner une autonomie réduite, des redémarrages intempestifs ou même un dysfonctionnement complet de l'appareil. Il est donc essentiel de surveiller l'état de la batterie et de la remplacer à temps pour éviter ces désagréments.ent de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            la batterie représentant une part importante des demandes en
+            atelier. Même si le téléphone semble fonctionner normalement, une
+            dégradation progressive de la batterie, due à des cycles de charge
+            répétés, une surchauffe ou une utilisation intensive, peut entraîner
+            une autonomie réduite, des redémarrages intempestifs ou même un
+            dysfonctionnement complet de l'appareil. Il est donc essentiel de
+            surveiller l'état de la batterie et de la remplacer à temps pour
+            éviter ces désagréments.
           </p>
         </div>
-
 
         <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             MICRO
           </h1>
           <Image
-            src="/images/parts/battery.png"
+            src="/images/parts/microphone.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-           la batterie représentant une part importante des demandes en atelier. Même si le téléphone semble fonctionner normalement, une dégradation progressive de la batterie, due à des cycles de charge répétés, une surchauffe ou une utilisation intensive, peut entraîner une autonomie réduite, des redémarrages intempestifs ou même un dysfonctionnement complet de l'appareil. Il est donc essentiel de surveiller l'état de la batterie et de la remplacer à temps pour éviter ces désagréments.ent de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            la batterie représentant une part importante des demandes en
+            atelier. Même si le téléphone semble fonctionner normalement, une
+            dégradation progressive de la batterie, due à des cycles de charge
+            répétés, une surchauffe ou une utilisation intensive, peut entraîner
+            une autonomie réduite, des redémarrages intempestifs ou même un
+            dysfonctionnement complet de l'appareil. Il est donc essentiel de
+            surveiller l'état de la batterie et de la remplacer à temps pour
+            éviter ces désagréments.ent de l'appareil photo d'un téléphone est
+            une réparation de plus en plus courante, notamment avec
+            l'utilisation intensive des fonctionnalités photographiques. Même si
+            l'objectif semble intact, des chocs, une exposition à l'humidité ou
+            une accumulation de poussière peuvent endommager les capteurs ou les
+            lentilles, entraînant des photos floues, des taches sur les images
+            ou un dysfonctionnement complet de la fonction photo. Ces problèmes
+            peuvent considérablement réduire la qualité des clichés et nuire à
+            l'expérience utilisateur. Il est donc important de diagnostiquer et
+            de remplacer l'appareil photo rapidement pour retrouver des
+            performances optimales et préserver la capacité à capturer des
+            moments précieux.
           </p>
         </div>
 
@@ -88,15 +128,33 @@ const Mobile = () => {
             HAUTS PARLEURS
           </h1>
           <Image
-            src="/images/parts/battery.png"
+            src="/images/parts/speakers.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-           la batterie représentant une part importante des demandes en atelier. Même si le téléphone semble fonctionner normalement, une dégradation progressive de la batterie, due à des cycles de charge répétés, une surchauffe ou une utilisation intensive, peut entraîner une autonomie réduite, des redémarrages intempestifs ou même un dysfonctionnement complet de l'appareil. Il est donc essentiel de surveiller l'état de la batterie et de la remplacer à temps pour éviter ces désagréments.ent de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            la batterie représentant une part importante des demandes en
+            atelier. Même si le téléphone semble fonctionner normalement, une
+            dégradation progressive de la batterie, due à des cycles de charge
+            répétés, une surchauffe ou une utilisation intensive, peut entraîner
+            une autonomie réduite, des redémarrages intempestifs ou même un
+            dysfonctionnement complet de l'appareil. Il est donc essentiel de
+            surveiller l'état de la batterie et de la remplacer à temps pour
+            éviter ces désagréments.ent de l'appareil photo d'un téléphone est
+            une réparation de plus en plus courante, notamment avec
+            l'utilisation intensive des fonctionnalités photographiques. Même si
+            l'objectif semble intact, des chocs, une exposition à l'humidité ou
+            une accumulation de poussière peuvent endommager les capteurs ou les
+            lentilles, entraînant des photos floues, des taches sur les images
+            ou un dysfonctionnement complet de la fonction photo. Ces problèmes
+            peuvent considérablement réduire la qualité des clichés et nuire à
+            l'expérience utilisateur. Il est donc important de diagnostiquer et
+            de remplacer l'appareil photo rapidement pour retrouver des
+            performances optimales et préserver la capacité à capturer des
+            moments précieux.
           </p>
         </div>
 
@@ -105,15 +163,33 @@ const Mobile = () => {
             BOUTON VOLUME
           </h1>
           <Image
-            src="/images/parts/battery.png"
+            src="/images/parts/volumebtn.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-           la batterie représentant une part importante des demandes en atelier. Même si le téléphone semble fonctionner normalement, une dégradation progressive de la batterie, due à des cycles de charge répétés, une surchauffe ou une utilisation intensive, peut entraîner une autonomie réduite, des redémarrages intempestifs ou même un dysfonctionnement complet de l'appareil. Il est donc essentiel de surveiller l'état de la batterie et de la remplacer à temps pour éviter ces désagréments.ent de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            la batterie représentant une part importante des demandes en
+            atelier. Même si le téléphone semble fonctionner normalement, une
+            dégradation progressive de la batterie, due à des cycles de charge
+            répétés, une surchauffe ou une utilisation intensive, peut entraîner
+            une autonomie réduite, des redémarrages intempestifs ou même un
+            dysfonctionnement complet de l'appareil. Il est donc essentiel de
+            surveiller l'état de la batterie et de la remplacer à temps pour
+            éviter ces désagréments.ent de l'appareil photo d'un téléphone est
+            une réparation de plus en plus courante, notamment avec
+            l'utilisation intensive des fonctionnalités photographiques. Même si
+            l'objectif semble intact, des chocs, une exposition à l'humidité ou
+            une accumulation de poussière peuvent endommager les capteurs ou les
+            lentilles, entraînant des photos floues, des taches sur les images
+            ou un dysfonctionnement complet de la fonction photo. Ces problèmes
+            peuvent considérablement réduire la qualité des clichés et nuire à
+            l'expérience utilisateur. Il est donc important de diagnostiquer et
+            de remplacer l'appareil photo rapidement pour retrouver des
+            performances optimales et préserver la capacité à capturer des
+            moments précieux.
           </p>
         </div>
 
@@ -122,19 +198,35 @@ const Mobile = () => {
             BOUTON ON/OF
           </h1>
           <Image
-            src="/images/parts/battery.png"
+            src="/images/parts/on-off.png"
             alt="ecran"
-            width={300}
-            height={300}
+            width={150}
+            height={150}
             className="  md:block"
           />
           <p className="text-[#1E3A8A] text-center text-2xl">
             {" "}
-           la batterie représentant une part importante des demandes en atelier. Même si le téléphone semble fonctionner normalement, une dégradation progressive de la batterie, due à des cycles de charge répétés, une surchauffe ou une utilisation intensive, peut entraîner une autonomie réduite, des redémarrages intempestifs ou même un dysfonctionnement complet de l'appareil. Il est donc essentiel de surveiller l'état de la batterie et de la remplacer à temps pour éviter ces désagréments.ent de l'appareil photo d'un téléphone est une réparation de plus en plus courante, notamment avec l'utilisation intensive des fonctionnalités photographiques. Même si l'objectif semble intact, des chocs, une exposition à l'humidité ou une accumulation de poussière peuvent endommager les capteurs ou les lentilles, entraînant des photos floues, des taches sur les images ou un dysfonctionnement complet de la fonction photo. Ces problèmes peuvent considérablement réduire la qualité des clichés et nuire à l'expérience utilisateur. Il est donc important de diagnostiquer et de remplacer l'appareil photo rapidement pour retrouver des performances optimales et préserver la capacité à capturer des moments précieux.
+            la batterie représentant une part importante des demandes en
+            atelier. Même si le téléphone semble fonctionner normalement, une
+            dégradation progressive de la batterie, due à des cycles de charge
+            répétés, une surchauffe ou une utilisation intensive, peut entraîner
+            une autonomie réduite, des redémarrages intempestifs ou même un
+            dysfonctionnement complet de l'appareil. Il est donc essentiel de
+            surveiller l'état de la batterie et de la remplacer à temps pour
+            éviter ces désagréments.ent de l'appareil photo d'un téléphone est
+            une réparation de plus en plus courante, notamment avec
+            l'utilisation intensive des fonctionnalités photographiques. Même si
+            l'objectif semble intact, des chocs, une exposition à l'humidité ou
+            une accumulation de poussière peuvent endommager les capteurs ou les
+            lentilles, entraînant des photos floues, des taches sur les images
+            ou un dysfonctionnement complet de la fonction photo. Ces problèmes
+            peuvent considérablement réduire la qualité des clichés et nuire à
+            l'expérience utilisateur. Il est donc important de diagnostiquer et
+            de remplacer l'appareil photo rapidement pour retrouver des
+            performances optimales et préserver la capacité à capturer des
+            moments précieux.
           </p>
         </div>
-
-
       </div>
     </div>
   );
