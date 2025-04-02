0a7fd8fa42b565be00acecd117218316e29a3624
thas93 <a.thas.dev93@gmail.com>
2025-04-03 00:23:56 +0200

    stadalone

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -12,166 +12,182 @@ const Mobile = () => {
       </div>
 
       <div className="p-4 flex flex-col justify-center items-center gap-6 w-screen h-full">
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             ECRAN
           </h1>
-          <Image
-            src="/images/parts/screen.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/screen.webp"
+              alt="screen"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             APPAREIL PHOTO
           </h1>
-          <Image
-            src="/images/parts/lens.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/camera.webp"
+              alt="camera"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             BATTERIE
           </h1>
-          <Image
-            src="/images/parts/batery.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/batery.png"
+              alt="battery"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
-            MICRO
+            MICROPHONE
           </h1>
-          <Image
-            src="/images/parts/microphone.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/microphone.png"
+              alt="microphone"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             HAUTS PARLEURS
           </h1>
-          <Image
-            src="/images/parts/speakers.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/speakers.png"
+              alt="speakers"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
             BOUTON VOLUME
           </h1>
-          <Image
-            src="/images/parts/volumebtn.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+              src="/images/parts/volumebtn.png"
+              alt="volumebtn"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center border-2 border-white rounded-lg p-4">
+        <div className="flex flex-col justify-center items-center border-2 border-yellow-500 rounded-lg p-4">
           <h1 className="text-2xl font-black text-[#1E3A8A] tracking-[0.3rem]">
-            BOUTON ON/OF
+            BOUTON ON/OFF
           </h1>
-          <Image
-            src="/images/parts/on-off.png"
-            alt="ecran"
-            width={150}
-            height={150}
-            className="  md:block"
-          />
-          <p className="text-[#1E3A8A] text-center text-2xl">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            illo labore excepturi officiis maxime ullam architecto non, harum
-            exercitationem expedita omnis facilis eius rem vitae illum ducimus
-            nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
-            neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
-            consequatur laboriosam voluptatibus possimus unde, illo enim
-            voluptate! Inventore delectus assumenda, esse possimus fugit officia
-            temporibus neque!
-          </p>
+          <div className="flex gap-10 rounded-lg justify-center items-center">
+            <Image
+             src="/images/parts/on-off.png"
+              alt="on-offbtn"
+              width={150}
+              height={150}
+              className="  md:block"
+            />
+            <p className="text-[#1E3A8A] text-center text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              illo labore excepturi officiis maxime ullam architecto non, harum
+              exercitationem expedita omnis facilis eius rem vitae illum ducimus
+              nemo esse. Doloremque labore obcaecati sapiente deserunt pariatur
+              neque maiores fugit perferendis modi quaerat! Ipsa cupiditate
+              consequatur laboriosam voluptatibus possimus unde, illo enim
+              voluptate! Inventore delectus assumenda, esse possimus fugit
+              officia temporibus neque!
+            </p>
+          </div>
         </div>
+
       </div>
     </div>
   );
